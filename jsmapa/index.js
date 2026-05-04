d06b5f03358f584061781256dea120b3a4d111ab
Alex Juvenal Huarancca Villasante <alexjuvenalh@gmail.com>
2026-05-04 18:33:38 -0500

    fix: expose window.map

--- jsmapa/index.js
+++ jsmapa/index.js
@@ -38,6 +38,9 @@
             zoom: 8,
         }).addLayer(osm);
 
+        // Exponer mapa globalmente para otros scripts
+        window.map = map;
+
         // Estilo para polígonos de Faja Marginal (rojo)
         var fajaStyle = {
             color: '#ff0000',
